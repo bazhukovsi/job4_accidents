f8944f464f00d8d074c18289c30c2e4774743ccb
bazhukovsi <bazhukovsi@yandex.ru>
2023-01-22 16:54:52 +0300

    3.1. @RequestParam. Форма редактирования [#308708]

--- src/main/java/job/accidents/controller/AccidentControl.java
+++ src/main/java/job/accidents/controller/AccidentControl.java
@@ -47,7 +47,7 @@ public class AccidentControl {
     }
 
     /**
-     * Редактирование инцидента
+     * Редактирование инцидента (все поля)
      * @param model
      * @param id
      * @return
@@ -61,7 +61,7 @@ public class AccidentControl {
             return "errors/404";
         }
         model.addAttribute("accident", accidentOptional.get());
-        return "accidents/saveAccident";
+        return "accidents/editAccident";
 
     }
 
@@ -96,4 +96,24 @@ public class AccidentControl {
         }
         return "redirect:/accidents";
     }
+
+    /**
+     * Редактирование инцидента (только название инцидента)
+     * @param model
+     * @param id
+     * @return
+     */
+
+    @GetMapping("/formUpdateAccident")
+    public String update(@RequestParam("id") int id, Model model) {
+        model.addAttribute("accident", accidentService.findById(id).get());
+        return "accidents/formUpdateAccident";
+    }
+
+    @PostMapping("/updateAccidentName")
+    public String save(@ModelAttribute Accident accident) {
+        accidentService.update(accident);
+        return "redirect:/index";
+    }
+
 }
